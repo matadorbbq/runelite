c958159790912c0a445277147a2c244cbce231f3
TheStonedTurtle <29030969+TheStonedTurtle@users.noreply.github.com>
2018-11-10 04:39:31 -0800

    LootTracker - Move PRICE ItemSortType to the end

--- runelite-client/src/main/java/net/runelite/client/plugins/loottracker/ItemSortTypes.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/loottracker/ItemSortTypes.java
@@ -9,8 +9,8 @@ public enum ItemSortTypes
 {
 	ALPHABETICAL("Alphabetical"),
 	ITEM_ID("Item ID"),
-	PRICE("PRICE"),
-	VALUE("Value");
+	VALUE("Value"),
+	PRICE("Price");
 
 	private final String name;
 
